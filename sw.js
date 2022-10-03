819fac8dabf91caa9ebc313f9019a59ef5a39fad
vimillan <20193tn066@utez.edu.mx>
2022-10-03 11:58:09 -0500

    feat: Añadida ruta en cahé para imágenes

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ self.addEventListener('install', (event) => {
                 '/css/style.css',
                 '/js/app.js',
                 '/js/fillData.js',
+                '/images/sucursal.png',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
                 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css'
