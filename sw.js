24babe0590223829a2794cd7621407000d8a7f9d
Romario Herrera <99297874+20183ti036@users.noreply.github.com>
2022-11-04 18:29:47 -0600

    feat: Creacion vistas de login y formulario de registro

--- sw.js
+++ sw.js
@@ -3,12 +3,19 @@ self.addEventListener('install', (event) => {
     const promiseCache = caches.open('cache-v1.1').then((cache) => {
         return cache.addAll(
             [
-                './',
-                './index.html',
-                './porfolio/estantes.html',
-                './porfolio/sucursales.html',
-                './css/style.css',
-                './js/app.js',
+                '/',
+                '/index.html',
+                '/login.html',
+                '/registro.html',
+                '/porfolio/estantes.html',
+                '/porfolio/sucursales.html',
+                '/css/estilos.css',
+                '/img/back.jpg',
+                '/img/fondo.png',
+                '/img/footer.png',
+                '/img/head.png',
+                '/img/headR.png',
+                '/js/app.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
             ]
