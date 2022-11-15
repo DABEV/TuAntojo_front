783c75972e07f9edcf1689165036b618b6a3724b
Romario Herrera <99297874+20183ti036@users.noreply.github.com>
2022-11-15 13:05:19 -0600

    feat: Agregada Conexion para Lista de sucursales

---
+++ js/stores-functions.js
@@ -0,0 +1,39 @@
+const BASE_URL = "http://localhost:8000/api/store";
+
+const getAllStores = () => {
+  const itemList = document.getElementById("itemList");
+  itemList.innerHTML = "";
+  fetch(`${BASE_URL}/index`)
+    .then((response) => response.json())
+    .then((data) => {
+      const stores = data.data;
+      stores.forEach((element) => {
+        console.log(element);
+        const card = document.createElement("div");
+        card.classList.add("col-6");
+        card.classList.add("col-md-4");
+        card.classList.add("col-lg-3");
+        card.classList.add("item");
+        card.innerHTML = `
+          <div class="item-icon">
+            <img src="../images/icons/store.png" class="img-fluid px-5 py-3" alt="">
+          </div>
+          <div class="px-2 py-2">
+            <div class="row">
+              <div class="col-1 d-flex align-items-center">
+                <i class="bx bxs-package ta-c-warn"></i>
+              </div>
+              <div class="col d-flex align-items-center">
+                <span class="fw-bold text-dark card-locale_title">${element.name}</span>
+              </div>
+            </div>
+            <span class="card-locale_content">${element.ubication}</span>
+          </div>
+        `;
+        card.addEventListener("click", () => {
+          console.log(element.id);
+        });
+        itemList.appendChild(card);
+      });
+    });
+};
