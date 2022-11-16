250d75233e692ce14dfbf1d5e2122ff2021d644e
SpawnSkull <saposuper_14@hotmail.com>
2022-11-16 11:09:26 -0600

    feat: Agregada conexión a registro de usuario

--- js/auth-functions.js
+++ js/auth-functions.js
@@ -30,7 +30,8 @@ const login = () => {
         localStorage.clear();
         localStorage.setItem("token", token);
         console.log("Registro de token");
-        window.location.href = "http://localhost:8080/porfolio/listaPedido.html"
+        window.location.href =
+          "http://localhost:8080/porfolio/listaPedido.html";
       } else {
         console.log("No se inició sesión");
       }
@@ -40,5 +41,57 @@ const login = () => {
     });
 };
 
+const register = () => {
+  const user = {
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  };
+
+  const nameUser = document.getElementById("name");
+  user.name = nameUser.value;
+
+  const emailUser = document.getElementById("email");
+  user.email = emailUser.value;
+
+  const passwordUser = document.getElementById("password");
+  user.password = passwordUser.value;
+
+  const passwordConfirmation = document.getElementById("passwordConfirm");
+  user.password_confirmation = passwordConfirmation.value;
+
+  fetch(`${BASE_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data) {
+        localStorage.clear();
+        console.log("Usuario creado");
+        window.location.href =
+          "http://localhost:8080/login.html";
+      } else {
+        console.log("Registro de usuario fallido");
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
 const btnLogin = document.getElementById("btnLogin");
-btnLogin.addEventListener("click", login);
+if(btnLogin){
+  btnLogin.addEventListener("click", login);
+}
+
+
+const btnRegister = document.getElementById("btnRegister");
+if(btnRegister){
+  btnRegister.addEventListener("click", register);
+}
