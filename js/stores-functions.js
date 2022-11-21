710c98203500bb56374ee61b46c8ea69cc279315
Abraham Rashid Cortez Morales <20193tn090@utez.edu.mx>
2022-11-21 17:51:00 -0600

    fix: Correción en el modal de comentarios

--- js/stores-functions.js
+++ js/stores-functions.js
@@ -1,4 +1,55 @@
 const BASE_URL = "http://localhost:8000/api/store";
+var tienda = 0;
+var nombre = "";
+
+const getAllStores = () => {
+  const itemList = document.getElementById("itemList");
+  itemList.innerHTML = "";
+  fetch(`${BASE_URL}/index`)
+    .then((response) => response.json())
+    .then((data) => {
+      const shelves = data.data;
+
+      shelves.forEach((element) => {
+        const card = document.createElement("div");
+        card.classList.add("row");
+        card.classList.add("item");
+        card.classList.add("mb-2");
+
+        card.innerHTML = `
+        <div class="row item mb-2">
+            <div class="col-3  col-lg-1 item-icon d-flex justify-content-center pt-2 mb-2">
+            <img src="../images/puesto-de-comida.png" class="img-fluid px-2 py-2">
+            </div>
+            <div class="col d-flex align-items-center">
+            <div>
+                <div class="fw-bold">${element.name}</div>
+                <div>No. de productos: <b class="badge rounded-pill ta-bg-soft-blue-1">12</b>
+                </div>
+            </div>
+            </div>
+            <div class="col-2 col-lg-1 d-flex align-items-center">
+            <button type="button" class="btn btn-icon" data-bs-toggle="modal" data-bs-target="#comentsModal">
+                <i class='bx bx-message-dots ta-c-teal-1 fs-1'></i>
+            </button>       
+            <div class="m-2">
+            <a href="../porfolio/productos.html"><i class="bx bx-cart ta-c-gray-icon-1 fs-1"></i>
+            </a>
+            </div>
+             
+            </div>
+        </div>
+    `;
+        card.addEventListener("click", () => {
+          console.log(element.id);
+//          window.location.href = "http://localhost:8080/porfolio/comentarios.html"
+        });
+        itemList.appendChild(card);
+      });
+    });
+};
+
+/*
 
 const getAllStores = () => {
   const itemList = document.getElementById("itemList");
@@ -37,3 +88,4 @@ const getAllStores = () => {
       });
     });
 };
+*/
--- sw.js
+++ sw.js
@@ -38,6 +38,7 @@ self.addEventListener("install", (event) => {
       `${INIT_BASE}porfolio/productos.html`,
       `${INIT_BASE}porfolio/estantes.html`,
       `${INIT_BASE}porfolio/comentarios.html`,
+      `${INIT_BASE}porfolio/listaTiendas.html`,
       `${INIT_BASE}images/icons/store.png`,
       `${INIT_BASE}images/candy1.png`,
       `${INIT_BASE}images/candy2.png`,
