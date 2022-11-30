23e59b80c945811e83d7d40a2a9f7dfbe448b673
Romario Herrera <99297874+20183ti036@users.noreply.github.com>
2022-11-30 00:18:32 -0600

    fix: Vista orden y listas de pedidos

--- js/orders-functions.js
+++ js/orders-functions.js
@@ -1,5 +1,5 @@
 const BASE_URL = "http://localhost:8000/api/order";
-
+var precioProducto =0;
 function isNotAuth() {
   try{
       var token = localStorage.getItem("token");
@@ -13,54 +13,111 @@ function isNotAuth() {
   }
 }
 
-const getAllOrders = () => {
+const getOrdersPendings = () => {
 
   isNotAuth();
 
   const itemList = document.getElementById("itemList");
   itemList.innerHTML = "";
   const id = localStorage.getItem('user');
-  fetch(`${BASE_URL}/findByUser/${id}`)
+  fetch(`${BASE_URL}/findByUserId/${id}`)
     .then((response) => response.json())
     .then((data) => {
       const orders = data.data;
-
+      var estado =""
       orders.forEach((element) => {
-        const card = document.createElement("div");
-        card.classList.add("row");
-        card.classList.add("item");
-        card.classList.add("mb-2");
-        card.innerHTML = `
-            <div class="row item mb-2">
-            <div class="col-3  col-lg-1 item-icon d-flex justify-content-center pt-2 mb-2">
-                <img src="../images/ticket.png" class="img-fluid px-2 py-2">
-            </div>
-            <div class="col d-flex align-items-center">
-                <div>
-                    <div class="fw-bold">${element.store_id}</div>
-                    <div class="">${element.product_id}</div>
-                    <div class="row">
-                        <div class="col text-end">
-                            <b class="badge-custom rounded-pill ta-c-danger">${element.status}</b>
-                        </div>
-                        <div class="col">
-                             <span>${element.amount}</span>
-                        </div>
+        if (element.status == 0){
+          const card = document.createElement("div");
+          card.classList.add("row");
+          card.classList.add("item");
+          card.classList.add("mb-2");
+          card.innerHTML = `
+              <div class="row item mb-2">
+              <div class="col-3  col-lg-1 item-icon d-flex justify-content-center pt-2 mb-2">
+                  <img src="../images/ticket.png" class="img-fluid px-2 py-2">
+              </div>
+              <div class="col d-flex align-items-center">
+                  <div>
+                      <div class="fw-bold">${element.store.name}</div>
+                      <div class="">${element.store.ubication}</div>
+                      <div class="row">
+                          <div class="col text-end">
+                              <b class="badge-custom rounded-pill ta-c-danger">Pendiente</b>
+                          </div>
+                          <div class="col">
+                              <span>$${element.payment}</span>
+                          </div>
 
-                    </div>
-                </div>
-            </div>
-            <div class="col-2 col-lg-1 d-flex align-items-center">
-                <button type="button" class="btn btn-icon">
-                    <i class='bx bx-check-circle ta-c-success'></i>
-                </button>
-            </div>
-        </div>
-        `;
-        card.addEventListener("click", () => {
-          console.log(element.id);
-        });
-        itemList.appendChild(card);
+                      </div>
+                  </div>
+              </div>
+              <div class="col-2 col-lg-1 d-flex align-items-center">
+                  <button type="button" class="btn btn-icon">
+                      <i class='bx bx-check-circle ta-c-success'></i>
+                  </button>
+              </div>
+          </div>
+          `;
+          card.addEventListener("click", () => {
+            console.log(element.id);
+          });
+          itemList.appendChild(card);
+        }
+      });
+    });
+};
+
+const getOrdersdelivered = () => {
+ console.log("entre");
+  isNotAuth();
+
+  const itemList = document.getElementById("itemList");
+  itemList.innerHTML = "";
+  const id = localStorage.getItem('user');
+  fetch(`${BASE_URL}/findByUserId/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const orders = data.data;
+      console.log(data.data);
+      var estado =""
+      orders.forEach((element) => {
+        if (element.status == 1){
+          const card = document.createElement("div");
+          card.classList.add("row");
+          card.classList.add("item");
+          card.classList.add("mb-2");
+          card.innerHTML = `
+              <div class="row item mb-2">
+              <div class="col-3  col-lg-1 item-icon d-flex justify-content-center pt-2 mb-2">
+                  <img src="../images/ticket.png" class="img-fluid px-2 py-2">
+              </div>
+              <div class="col d-flex align-items-center">
+                  <div>
+                      <div class="fw-bold">${element.store.name}</div>
+                      <div class="">${element.store.ubication}</div>
+                      <div class="row">
+                          <div class="col text-end">
+                              <b class="badge-custom rounded-pill ta-c-danger">Entregado</b>
+                          </div>
+                          <div class="col">
+                              <span>$${element.payment}</span>
+                          </div>
+
+                      </div>
+                  </div>
+              </div>
+              <div class="col-2 col-lg-1 d-flex align-items-center">
+                  <button type="button" class="btn btn-icon">
+                      <i class='bx bx-check-circle ta-c-success'></i>
+                  </button>
+              </div>
+          </div>
+          `;
+          card.addEventListener("click", () => {
+            console.log(element.id);
+          });
+          itemList.appendChild(card);
+        }
       });
     });
 };
@@ -76,6 +133,7 @@ const addOrder = () => {
       };
     const cant = document.getElementById("numUnidades");
     order.amount = cant.value;
+    order.payment = cant.value * precioProducto;
     order.status = 0;
     order.store_id = localStorage.getItem('store_id');
     order.product_id = localStorage.getItem('product_id');
@@ -93,11 +151,11 @@ const addOrder = () => {
           const order = data.data;
           if (order) {
             swal({
-                title: "se registro",
-                text: "You clicked the button!",
-                icon: "success",
-              });
-            window.location.href = "http://localhost:8080/porfolio/productos.html";
+              title: "se registro tu orden",
+            })
+            setTimeout(function(){
+              window.location.replace("http://localhost:8080/porfolio/listapedido.html");
+          }, 2000); 
           } else {
             swal({
                 title: "no se registro",
@@ -117,6 +175,7 @@ const getOneProduct = () =>{
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+    precioProducto = data.data.price;
     const datos = document.getElementById("datosProducto");
     datos.innerHTML = `
     <strong>${data.data.name}</strong>
